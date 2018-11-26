97c27cd4e5f6f6a2a96ffd5f308b78322b31e7e8
poeigame <45357821+poeigame@users.noreply.github.com>
2018-11-26 19:17:48 +0700

    Add files via upload

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'zREapw+JZ8kaDVAlz4Kq9gKVBpn646X8I1Gcq/0JO8zqegDtj24r3H81ceiviAkTSqGOdfERg3Ks4pBMw70S1BVbDqfDnKzXgt8hgQ0YoAWwumtNglT0tWQn/ID7kE8kJJRFlSwHeb2J+htYUCn+4gdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
