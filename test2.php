8ace120a06094f92515a2c1ac388e8c7b6522ce3
poeigame <45357821+poeigame@users.noreply.github.com>
2018-11-28 10:49:19 +0700

    Add files via upload

--- test2.php
+++ test2.php
@@ -55,7 +55,7 @@
         $arrayPostData['messages'][1]['stickerId'] = "131";
         replyMsg($arrayHeader,$arrayPostData);
     }
-	else if(strpos($message,"มิวนิค")){
+	else if(strpos($message,"มิวนิค") != null){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "อย่าบด !";
